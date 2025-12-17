72005b604674344d45050d6e6c335e7b16725b12
Jie Zhao <zj982853200@dlut.edu.cn>
2025-12-17 11:18:51 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -4,6 +4,10 @@
 
 script='savlt'
 config='SAVLT-B'
+
+python -m torch.distributed.launch --nproc_per_node 2 --script ${script} --config ${config} --save_dir YOUR_SAVE_PATH
+
+
 python tracking/test.py ${script} ${config} --dataset lasot_ext_lang --threads 2
 python tracking/test.py ${script} ${config} --dataset otb99_lang --threads 2
 python tracking/test.py ${script} ${config} --dataset lasot_lang --threads 2
